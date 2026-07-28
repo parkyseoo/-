71fed0482c06e3471f55a79c008c8b3b10be92ab
parkyseoo <gsfvv8ctjz@privaterelay.appleid.com>
2026-07-29 00:00:30 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SmartPlateApp from './SmartPlateApp.jsx' // App.jsx 대신 SmartPlateApp을 연결!
+import SmartPlateApp from './SmartPlateApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
