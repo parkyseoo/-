249ba6bb486c9fba93cbb702dd6c2ad72778dc3d
parkyseoo <gsfvv8ctjz@privaterelay.appleid.com>
2026-07-29 00:21:06 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SmartPlateApp from './SmartPlateApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SmartPlateApp />
+  </React.StrictMode>,
+)
